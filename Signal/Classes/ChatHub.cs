a81fa24227d5bde7372301faa80e778d40c787d1
shamsipav <aseevpav@gmail.com>
2022-12-29 09:03:09 +0500

    Добавьте файлы проекта.

---
+++ Signal/Classes/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Signal.Classes
+{
+    public class ChatHub: Hub
+    {
+        static List<User> Users = new List<User>();
+
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Receive", message, userName);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            string id = Context.ConnectionId;
+
+            if (!Users.Any(x => x.ConnectionId == id))
+            {
+                Users.Add(new User { ConnectionId = id });
+            }
+
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат", Users);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var user = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+
+            if (user != null)
+            {
+                Users.Remove(user);
+            }
+
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} покинул чат", Users);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Signal/Program.cs
@@ -0,0 +1,14 @@
+using Signal.Classes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
